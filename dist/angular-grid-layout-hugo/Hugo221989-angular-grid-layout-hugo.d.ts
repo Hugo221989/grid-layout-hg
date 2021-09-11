540a88cd48c8a3bd86da544072e00d979d853741
horlando <horlando@eservicios.indra.es>
2021-09-11 16:49:19 +0200

    a

---
+++ dist/angular-grid-layout-hugo/Hugo221989-angular-grid-layout-hugo.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Generated bundle index. Do not edit.
+ */
+export * from './public-api';
+export { KtdGridService as ɵb } from './lib/grid.service';
+export { compact as ɵa } from './lib/utils/react-grid-layout.utils';
--- dist/angular-grid-layout/katoid-angular-grid-layout.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Generated bundle index. Do not edit.
- */
-/// <amd-module name="@katoid/angular-grid-layout" />
-export * from './public-api';
-//# sourceMappingURL=katoid-angular-grid-layout.d.ts.map
